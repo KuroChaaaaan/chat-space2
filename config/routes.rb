1c8348dbd0f1c818ecc9a92de47775cdbd17e0de
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-04 16:38:32 +0900

    modelSetUp_groups_groups_users_View-Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, onry: [:edit, :update]
+  resources :groups, onry: [:new, :create, :edit, :update]
+    resources :messages, onry: [:index, :create]
 end
