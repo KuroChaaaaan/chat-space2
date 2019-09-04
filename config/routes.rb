6f36e16bba4e79c2605af24efb092d5833af0d7c
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-04 20:20:53 +0900

    User_Management-Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'messages#index'
+  root to: 'groups#index'
   resources :users, onry: [:edit, :update]
-  resources :groups, onry: [:new, :create, :edit, :update]
+  resources :groups, onry: [:new, :create, :edit, :update] do
     resources :messages, onry: [:index, :create]
+  end
 end
