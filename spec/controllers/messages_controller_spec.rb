d4e3aeef6dbbbb78b13d64ea085633c8ddd408eb
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-06 16:22:17 +0900

    messageControllerTest2-Commit

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe MessagesController do
   let(:group) { create(:group) }
   let(:user) { create(:user) }
+  let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
 
   describe '#index' do
     context 'log in' do
@@ -30,6 +31,59 @@ describe MessagesController do
       end
 
       it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end  
+  end
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+    context 'log in' do
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context 'can not save' do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, message: nil, image: nil) } }
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          # Messageモデルのレコード数が変化しないこと ≒ 保存に失敗したこと」を確かめる
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'renders index' do
+          # メッセージの保存に失敗した場合、indexアクションのビューをrenderするよう設定
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    # 未login時にcreateアクションをリクエストした際は、ログイン画面へとリダイレクト。
+    # redirect_toマッチャの引数に、new_user_session_pathを取り、ログイン画面へとリダイレクトしているかどうかを確かめる
+    context 'not log in' do
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
         expect(response).to redirect_to(new_user_session_path)
       end
     end
