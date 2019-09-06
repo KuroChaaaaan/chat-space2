8e7ce037b8c2b3e6cda7a6d5b9e8d15ac6d8afe8
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-06 15:17:09 +0900

    messagesControllerTest-Commit

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :message do
     message {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
+    # image {File.open("#{Rails.root}/app/assets/images/")}
+    image {File.open("/Users/taishi.k/Desktop/kaiba-man/08732d24be0e649626b91d2e77a40014.jpg")}
     user
     group
   end
