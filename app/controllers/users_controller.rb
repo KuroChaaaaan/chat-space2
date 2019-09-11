8a59aca928779223b1c8981b41533efbec0afea0
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-11 15:38:06 +0900

    Incremental_search_implementation-Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
+  
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -10,8 +20,7 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
---
+++ app/views/users/index.json.jbuilder
@@ -0,0 +1,4 @@
+json.array! @users do |invitation|
+  json.id invitation.id
+  json.name invitation.name
+end
